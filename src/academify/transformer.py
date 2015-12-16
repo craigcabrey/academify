50d4e32b36579c10eead0c8cba5e13dff339a598
Craig Cabrey <craigcabrey@gmail.com>
2015-12-16 00:29:25 -0500

    Tokenize input into list of sentences

--- src/academify/transformer.py
+++ src/academify/transformer.py
@@ -1,9 +1,11 @@
+import nltk.tokenize
+
 class Transformer():
     def __init__(self, mode, length, input, output):
-        pass
+        self.buffer = ''.join(input.readlines())
 
     def transform(self):
-        pass
+        sentences = nltk.tokenize.sent_tokenize(self.buffer)
 
     def save(self):
         pass
