a9d628b924930828cf56da53ff2d9244aca2ef30
Craig Cabrey <craigcabrey@gmail.com>
2015-12-17 20:31:15 -0500

    Implement rudimentary algorithm

--- src/academify/transformer.py
+++ src/academify/transformer.py
@@ -1,4 +1,6 @@
 from .fluff import transitions
+import nltk
+import nltk.corpus
 import nltk.tokenize
 
 
@@ -9,6 +11,7 @@ class Transformer():
         self.target_length = length
         self.output = output
         self.buffer = ''
+        self.transitions = transitions()
 
     def transform(self):
         sentences = nltk.tokenize.sent_tokenize(self.source)
@@ -16,8 +19,44 @@ class Transformer():
 
         while cur_len < self.target_length:
             for sentence in sentences:
+                # TODO: develop sentence-level blacklist
+                if '%%' in sentence:
+                    self.buffer += sentence
+                    continue
+
+                words = nltk.tokenize.word_tokenize(sentence)
+
+                # TODO: develop word-level blacklist
+                # Extract only adjectives ('JJ')
+                adjectives = [
+                    tag[0] for tag in nltk.pos_tag(words)
+                        if tag[1] == 'JJ' and not tag[0].startswith('\\')
+                ]
+
+                replacement_adjectives = {}
+                for adjective in adjectives:
+                    synonyms = set()
+                    synonym_sets = nltk.corpus.wordnet.synsets(adjective)
+                    for synonym_set in synonym_sets:
+                        synonyms.update(synonym_set.lemma_names())
+
+                    if len(synonyms) > 0:
+                        replacement_adjectives[adjective] = max(synonyms, key=len)
+
                 self.buffer += sentence
+
+                if len(replacement_adjectives) > 0:
+                    fluff = sentence[0].lower() + sentence[1:]
+                    for original, replacement in replacement_adjectives.items():
+                        fluff = fluff.replace(original, replacement)
+
+                    fluff = next(self.transitions) + ', ' + fluff
+
+                    # Time to make our audience proud
+                    self.buffer += fluff
+
             cur_len = len(self.buffer)
 
     def save(self):
         self.output.write(self.buffer)
+
